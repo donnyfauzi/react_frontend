4fb53ca91fef6b8ed82edc43579b3a9755a462ba
donnyfauzi <donnyfauzi55@gmail.com>
2025-01-18 16:10:57 +0700

    update

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from "react-dom/client"
+import App from './App'
 
-const app = () => {
+// Cari elemen root
+const rootElement = document.getElementById('root')
 
-}
+// Buat root dengan React 18
+const root = ReactDOM.createRoot(rootElement)
 
-ReactDOM.render(<App />, document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
