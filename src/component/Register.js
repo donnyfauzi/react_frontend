418b224655f14590afadab5f8b3cf0dc28475062
donnyfauzi <donnyfauzi55@gmail.com>
2025-02-01 17:05:21 +0700

    update

--- src/component/Register.js
+++ src/component/Register.js
@@ -1,7 +1,10 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import "../styles/registerLogin.css";
 import axios from "axios";
+
+import { ReactNotifications, Store } from "react-notifications-component";
+import "react-notifications-component/dist/theme.css"; // Import style-nya
 
 const Register = () => {
   const [formData, setFormData] = useState({
@@ -10,17 +13,30 @@ const Register = () => {
     password: "",
     confirmPassword: "",
   });
-  const [message, setMessage] = useState("");
   const navigate = useNavigate(); // Untuk redirect setelah berhasil
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
+  const handleShowNotification = (type, message) => {
+    Store.addNotification({
+      title: type === "success" ? "Success!" : "Error!",
+      message: message,
+      type: type, // 'success', 'danger', 'info', 'warning'
+      insert: "top-center",
+      container: "top-center",
+      animationIn: ["animated", "fadeIn"],
+      animationOut: ["animated", "fadeOut"],
+      duration: 3000,
+    });
+  };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
+
     if (formData.password !== formData.confirmPassword) {
-      setMessage("Passwords do not match!");
+      handleShowNotification("danger", "Passwords do not match!");
       return;
     }
 
@@ -29,17 +45,25 @@ const Register = () => {
         "http://localhost:5000/api/register",
         formData
       );
-      setMessage("Registration successful!");
-      setTimeout(() => navigate("/login"), 2000); // Redirect ke login setelah 2 detik
+
+      handleShowNotification("success", response.data.message);
+
+      setTimeout(() => {
+        navigate("/login");
+      }, 3000); // Redirect setelah 3 detik
     } catch (error) {
-      setMessage(error.response?.data?.message || "Registration failed!");
+      handleShowNotification(
+        "danger",
+        error.response?.data?.message || "Something went wrong!"
+      );
     }
   };
 
   return (
     <div className="register-container">
+      {/* Menampilkan notifikasi */}
+      <ReactNotifications />
       <h2>Register</h2>
-      {message && <p>{message}</p>}
       <form onSubmit={handleSubmit}>
         <div className="form-group">
           <label>Name:</label>
